a6780cfb4a3316c78242edfc3236cd1fbc8c552b
carloschan111@gmail.com <carloschan111@gmail.com>
2023-01-18 20:22:28 +0800

    vuerd test set

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
 import express from "express";
 import { Request, Response } from "express";
 // import expressSession from "express-session";
+import { Client } from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const client = new Client({
+  database: process.env.DB_NAME,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+});
 
 const app = express();
+client.connect();
 
 declare module "express-session" {
     interface SessionData {
---
+++ pj2.sql
@@ -0,0 +1,8 @@
+insert into users (name,password,registered_date,status_admin,role_id,email) values('carlos','123456','2023-01-18',true,'1','carloschan111');
+insert into users (name,password,registered_date,status_admin,role_id,email) values('kevin','123456','2023-01-18',true,'1','kevin123');
+
+insert into users (name,password,registered_date,status_admin,role_id,email) values('bigwolfdog','1234','2023-01-18',false,'2','bigwolfdog222');
+insert into users (name,password,registered_date,status_admin,role_id,email) values('doWhatSpring','123321','2023-01-18',false,'2','dws123321');
+insert into users (name,password,registered_date,status_admin,role_id,email) values('stepJfindMay','654321','2023-01-18',false,'2','stepJJ123');
+insert into users (name,password,registered_date,status_admin,role_id,email) values('7day7head','123123','2023-01-18',false,'1','ddd777ddd');
+
